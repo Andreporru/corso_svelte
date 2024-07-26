dea34d49f142d6bf18249172e96846f558584e43
andrea porru <porruandre3@gmail.com>
2024-07-26 11:40:48 +0200

    nuovo test02

---
+++ test02/src/lib/components/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Page from '../../routes/+page.svelte';
+
+	interface Props {
+		label: string;
+		bgColor?: string;
+		color?: string;
+		// events
+		onclick?: () => void;
+		// ...rest
+		[key: string]: any;
+	}
+
+	let { label, bgColor, color, onclick, ...rest }: Props = $props();
+</script>
+
+<button style:background-color={bgColor} style:color {onclick} {...rest}>ciao mondo</button>
+
+<style>
+	button {
+		background-color: red;
+	}
+</style>
---
+++ test02/src/routes/button/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+	import Layout from '$lib/components/Layout.svelte';
+	const onclick = () => {
+		console.log('=== clicked');
+	};
+	const onfocus = () => {
+		console.log('=== focused');
+	};
+	const onhover = (elem: HTMLElement) => {
+		console.log('===hovered', elem);
+		elem.style.backgroundColor = 'yellow';
+	};
+	let valore: boolean = $state(false);
+	//    let dir:'column' | 'row' = $derived(vertical? 'column' : 'row');
+</script>
+
+<input type="checkbox" id="valore" name="valore" bind:checked={valore} />
+<label for="vehicle1"> verticale</label><br />
+
+<Layout direction={valore ? 'column' : 'row'} {onhover} style="border:1px solid #ccc; padding: 8px">
+	<!-- se ue valore è true da column se è false da row-->
+	<Button label="Ciao mondo" />
+	<Button label="Ciao mondo" bgColor="blueviolet" />
+	<Button label="Ciao mondo" bgColor="blueviolet" color="white" {onclick} {onfocus} />
+</Layout>
